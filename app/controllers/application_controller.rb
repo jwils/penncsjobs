e75518725a157f36f6b3570b8ce3287ff993bec8
Josh Wilson <jwils@seas.upenn.edu>
2013-04-01 17:13:01 -0400

    Require login and build confirmations controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :require_login
+
+  private
+
+  def require_login
+    unless current_user or devise_controller?
+      redirect_to new_user_session_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ JobBoard::Application.routes.draw do
   resources :companies
 
 
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
+  devise_scope :user do
+    get '/confirmation-getting-started' => 'registrations#getting_started', as: 'confirmation_getting_started'
+  end
+
+  resources :users
 
   root :to => 'job_postings#index'
 
